df15d17aa455b6a297af4a57126468809df53dba
Thanapong Rakkaew <thanapong040@gmail.com>
2021-02-18 21:13:39 +0700

    Edit Main class

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main{
     public static void main(String[] args){
         Company c = new Company("CoE");
-        
+        c.addEmployee(new Fulltime("Jane", 30000));
+        c.addEmployee(new Salesman("John", 10000, 150000));
+        c.addEmployee(new Fulltime("Randy", 18000));
+        c.addEmployee(new Parttime("Josh", 150, 80));
+        c.addEmployee(new Parttime("Marry", 100, 60));
         c.print();        
     }
 }
